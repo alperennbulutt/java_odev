53caa80ccdfa8ccbf3f85f2d82fe42b168ca1c7c
cemilebeyza duzen <beyzaaduzen@gmail.com>
2021-05-16 00:45:35 +0300

    second commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module java.odev {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.xml;
+    requires jdk.compiler;
+
+    opens sample;
+}
--- src/sample/Parser.java
+++ src/sample/Parser.java
@@ -1,15 +1,22 @@
 package sample;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.DocumentBuilder;
-import java.io.File;
-import java.io.IOException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.*;
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentType;
+import org.w3c.dom.Entity;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 
 
 public class Parser  {
+
 
 
 
